5b0b37e284e88bd98ef788bc42be41a2647ea7c7
trojan1771 <bt20cse141@iiitn.ac.in>
2024-03-27 15:46:05 +0530

    final

---
+++ src/components/images.js
@@ -0,0 +1,12 @@
+const images = {
+  'img/product-1.png': require('../assets/img/product-1.png'),
+  'img/product-2.png': require('../assets/img/product-2.png'),
+  'img/product-3.png': require('../assets/img/product-3.png'),
+  'img/product-4.png': require('../assets/img/product-4.png'),
+  'img/product-5.png': require('../assets/img/product-5.png'),
+  'img/product-6.png': require('../assets/img/product-6.png'),
+  'img/product-7.png': require('../assets/img/product-7.png'),
+  'img/product-8.png': require('../assets/img/product-8.png'),
+}
+
+export default images
